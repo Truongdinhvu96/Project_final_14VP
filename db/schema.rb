b7c06e62395df891e42c884e0bff3843c452f208
BuiQueLien <buiquelien.zone@gmail.com>
2017-01-14 18:21:28 +0700

    data base_Set cart, line-item(contraint)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114100151) do
+ActiveRecord::Schema.define(version: 20170114110928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -24,8 +36,10 @@ ActiveRecord::Schema.define(version: 20170114100151) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "types", force: :cascade do |t|
-    t.string "nom"
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
