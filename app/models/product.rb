33d22297fafdb621075e0b72a2ed9fcfedc33305
Hung <hungcao1216@gmail.com>
2017-01-15 22:49:18 +0700

    Tạo chức năng thêm giỏ hàng

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,17 @@ class Product < ApplicationRecord
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.0}
   validates :title, :uniqueness => true
+
+  has_many :line_items
+  before_destroy :check_if_has_line_item
+private
+
+    def check_if_has_line_item
+        if line_items.empty?
+            return true
+        else
+            errors.add(:base, 'This product has a LineItem')
+            return false
+        end
+    end
 end
